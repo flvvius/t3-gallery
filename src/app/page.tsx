06568c4faff66d5e4b487154235e5d7888d6c883
flavius <flaviuscojocaru19@gmail.com>
2024-09-10 12:31:06 +0300

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
-  "https://utfs.io/f/73ace47e-183d-4c59-8b1c-f34132c46321-nvu2p0.jpg",
-  "https://utfs.io/f/01899074-e841-456d-a951-70793e6807a4-b9b0jk.jpg",
-  "https://utfs.io/f/5609c14c-848f-4940-b6c4-976ec7c16209-4a5lxs.jpg",
-  "https://utfs.io/f/fc4f2321-0186-4c56-90e0-063981d0718f-5onyam.jpg",
+  "https://images.unsplash.com/photo-1422493757035-1e5e03968f95?q=80&w=2880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1495616811223-4d98c6e9c869?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1474524955719-b9f87c50ce47?q=80&w=3272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1429734956993-8a9b0555e122?q=80&w=3304&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
 const mockImages = mockUrls.map((url, index) => ({
@@ -12,12 +13,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
